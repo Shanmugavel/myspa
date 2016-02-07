a8ba99b33ddebbd569bd83b48ab208b954b4d8da
Shan <shanmugavel.rs@gmail.com>
2016-02-06 20:53:44 -0700

    Draft Version!!!

---
+++ README.md
@@ -0,0 +1,4 @@
+My SPA!
+=======
+
+Sample project to integrate with stormpath identity API's and explore oauth 2 and jwt.
---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-spa',
+    template: '<h1>App under construction. Please check back later.</h1>'
+})
+
+export class AppComponent { }
