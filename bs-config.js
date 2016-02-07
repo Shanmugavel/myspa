6faf03b6484913bd3612c40bbaaa3bf51573204c
Shan <shanmugavel.rs@gmail.com>
2016-02-07 01:07:13 -0700

    chaging port for lite-server

--- bs-config.js
+++ bs-config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  server: {
-    port : process.env.PORT,
-    //port: 8080,
+
+"port" : process.env.PORT
+
+/*  server: {
     middleware: {
       // overrides the second middleware default with new settings
       1: require('connect-history-api-fallback')({index: '/index.html', verbose: true})
     }
-  }
+  }*/
 };
