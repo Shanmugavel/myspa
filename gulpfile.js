1243533b87e197fc12514b24a400d7bbd951ff9f
Shan <shanmugavel.rs@gmail.com>
2016-02-17 23:02:25 -0700

    Integrated with bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,25 @@
-var gulp = require('gulp'), webserver = require('gulp-webserver');
+var gulp = require('gulp'), del = require('del');
 var exec = require('child_process').exec;
+
 gulp.task('default', ['pack-static', 'start-node']);
 
-gulp.task('webserver', function () {
-    gulp.src('.')
-        .pipe(webserver({
-            livereload: true,
-            open: true
-        }));
-});
 
 gulp.task('pack-static', function() {
-  gulp.src('./node_modules/bootstrap/dist/css/*.min.css').pipe(gulp.dest('./client/css/'));
-  gulp.src('./node_modules/bootstrap/dist/css/*.map').pipe(gulp.dest('./client/css/'));
-  gulp.src('./node_modules/bootstrap/dist/js/*.min.js').pipe(gulp.dest('./client/js/'));
-  gulp.src('./node_modules/bootstrap/dist/js/*.map').pipe(gulp.dest('./client/js/'));
-  gulp.src('./node_modules/bootstrap/dist/fonts/*').pipe(gulp.dest('./client/fonts/'));
-  gulp.src('./node_modules/es6-shim/*.min.js').pipe(gulp.dest('./client/js/'));
-  gulp.src('./node_modules/es6-shim/*.map').pipe(gulp.dest('./client/js/'));
-  gulp.src('./node_modules/systemjs/dist/*.js').pipe(gulp.dest('./client/js/'));
-  gulp.src('./node_modules/systemjs/dist/*.map').pipe(gulp.dest('./client/js/'));
-  gulp.src('./node_modules/angular2/bundles/*.js').pipe(gulp.dest('./client/js/'));
-  gulp.src('./node_modules/rxjs/bundles/*.js').pipe(gulp.dest('./client/js/'));
-  gulp.src('./node_modules/jquery/dist/*.min.js').pipe(gulp.dest('./client/js/'));
+  
+  var child = exec( 'bower-installer --remove', {}, function (err, stdout, stderr) {
+      gulp.src('./client/system.js/css/system-polyfills.js.map').pipe(gulp.dest('./client/system.js/js/'));
+      del(['./client/system.js/css']);
+  });
+  child.stdout.on('data', function(data) {
+        console.log('stdout: ' + data);
+  });
+  child.stderr.on('data', function(data) {
+        console.log('stderr: ' + data);
+  });
 });
 
 gulp.task('start-node', function() {
    var child =  exec('node server/server.js', { env: {'PORT' : '3000', 'NODE_CONFIG_DIR' : './server/config'} },  function (err, stdout, stderr) {
-        //console.log(stdout);
-        //console.log(stderr);
     });
 child.stdout.on('data', function(data) {
     console.log('stdout: ' + data);
